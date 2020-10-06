9c09374e0cd0468d4da94adf361e2ae7469ff530
Aiperi <aip.amankulova@gmail.com>
2020-10-06 19:59:43 +0600

    Adding an interface to the project

---
+++ app/src/main/java/com/example/acinema/data/remote/ImdbService.kt
@@ -0,0 +1,13 @@
+package com.example.acinema.data.remote
+
+import com.example.acinema.data.model.CinemaSearchModel
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ImdbService {
+
+    @GET(".")
+    fun getCinema(@Query("apikey") apiKey: String, @Query("s") title: String):
+            Call<CinemaSearchModel>
+}
